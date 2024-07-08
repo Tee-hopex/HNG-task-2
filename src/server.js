00f43f251b5116134a7140547112becd95b77db1
Tee-hopex <afolayanthopex@gmail.com>
2024-07-08 12:24:48 +0100

    vercel update

--- src/server.js
+++ src/server.js
@@ -1,3 +1,25 @@
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const { sequelize } = require('./models');
+// const authRoutes = require('./routes/authRoutes');
+// const orgRoutes = require('./routes/orgRoutes');
+
+// const app = express();
+// const port = process.env.PORT || 5000;
+
+// app.use(bodyParser.json());
+// app.use('/auth', require('./routes/authRoutes'));
+// app.use('/api/organisations', require('./routes/orgRoutes'));
+
+// sequelize.sync().then(() => {
+//   app.listen(port, () => {
+//     console.log(`Server is running on port ${port}`);
+//   });
+// });
+
+// module.exports = app;
+
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const { sequelize } = require('./models');
@@ -5,16 +27,18 @@ const authRoutes = require('./routes/authRoutes');
 const orgRoutes = require('./routes/orgRoutes');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use('/auth', require('./routes/authRoutes'));
-app.use('/api/organisations', require('./routes/orgRoutes'));
+app.use('/auth', authRoutes);
+app.use('/api/organisations', orgRoutes);
 
 sequelize.sync().then(() => {
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
+  if (process.env.NODE_ENV !== 'test') {
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  }
 });
 
-module.exports = app;
+module.exports = app; // Export the app for Vercel
